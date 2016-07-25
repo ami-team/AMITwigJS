9c594406267c96d862ca429ab2d22f198f54aca8
Jérôme Odier <odier.jerome@gmail.com>
2016-07-25 17:10:39 +0200

    update

--- dist/ami-twig.js
+++ dist/ami-twig.js
@@ -2198,12 +2198,6 @@ amiTwig.engine = {
 	{
 		var result = [];
 
-if (! ('console' in window) || !('firebug' in console)) {
-    var names = ['log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml', 'group', 'groupEnd', 'time', 'timeEnd', 'count', 'trace', 'profile', 'profileEnd'];
-    window.console = {};
-    for (var i = 0; i < names.length; ++i) window.console[names[i]] = function() {};
-}
-
 		this._render(result, Object.prototype.toString.call(tmpl) === '[object String]' ? this.compile(tmpl) : tmpl, dict || {});
 
 		return result.join('');
