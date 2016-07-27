d3e606a5fe98425ab448323e4792e70586dff3ad
Jérôme Odier <odier.jerome@gmail.com>
2016-07-27 11:39:48 +0200

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
  * Version: 0.2.0
  *
  * Copyright (c) 2014-2015 The AMI Team
+ *
+ * This file must be used under the terms of the CeCILL-C:
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
  *
  */
