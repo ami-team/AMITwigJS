dbf4c3fba99fe805969e4a0d6a9772004b4aebf6
Jérôme Odier <odier.jerome@gmail.com>
2016-07-27 11:40:29 +0200

    update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@
  *
  * This file must be used under the terms of the CeCILL-C:
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
  *
  */
 
