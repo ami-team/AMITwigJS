c1804db031c9d9c38b6cc1a4b895b5bee49d64ea
Jérôme Odier <odier.jerome@gmail.com>
2016-07-27 11:41:50 +0200

    update

--- src/compiler.js
+++ src/compiler.js
@@ -2,7 +2,10 @@
  * AMI TWIG Engine
  *
  * Copyright (c) 2014-2015 The AMI Team
+ *
+ * This file must be used under the terms of the CeCILL-C:
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
  *
  */
 
--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 /*
  * AMI TWIG Engine
  *
- * Version: 0.1.0
- *
  * Copyright (c) 2014-2015 The AMI Team
+ *
+ * This file must be used under the terms of the CeCILL-C:
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
  *
  */
 
